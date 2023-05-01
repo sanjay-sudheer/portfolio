ee6d8d6d6563a777558d902db982aa5943321acc
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2023-05-01 18:53:44 +0530

    v1.1

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-gsap.from('.landing',{opacity:0, duration:1, x: -50})
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.from('.landing',{opacity:0, duration:2, x: -50});
+gsap.from('.about',{scrollTrigger: {
+  trigger: '.about',
+  start:'-60% top',
+  markers: true,
+  scrub: 1.5,
+  end: '100px',
+  toggleActions: "play restart none none"
+},opacity:0, duration:1});
+
