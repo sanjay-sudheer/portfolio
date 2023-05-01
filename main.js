d6f9a3deb1f17febac8c1e437e8f3bee469d1d44
sanjay-sudheer <sanjaysudheer2003@gmail.com>
2023-05-01 19:27:11 +0530

    v1.1

--- main.js
+++ main.js
@@ -3,10 +3,19 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.from('.landing',{opacity:0, duration:2, x: -50});
 gsap.from('.about',{scrollTrigger: {
   trigger: '.about',
-  start:'-60% top',
+  start:'-60% 20%',
+  end:'200px 300px',
+  scrub: 1.5,
+  toggleActions: "play none none none"
+},opacity:0, duration:1});
+
+
+gsap.from('.projects',{scrollTrigger: {
+  trigger: '.projects',
+  start:'-30% 60%',
+  end: '200px 70%',
   markers: true,
   scrub: 1.5,
-  end: '100px',
-  toggleActions: "play restart none none"
-},opacity:0, duration:1});
+  toggleActions: "play none none none"
+},opacity:0, duration:1, y:200});
 
